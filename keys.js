bce54d94b00d9eac1a73da5f5e09a255bbd5bb28
Danish Akhtar <akhtar@Danishs-MacBook-Pro.local>
2018-01-12 02:31:28 -0800

    initial commit with skeleton

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'PZUU1VN5J05TdE0SOND7eg6IT',
+  consumer_secret: 'yyk5eFvEGiO3bJEGqpR6XCOOBGSaKBshQIR85FLECJaE6npfPn',
+  access_token_key: '3051749148-TtdHdqWWuVn4aELBJSm4I8zahNtJZuTnVpQWuT',
+  access_token_secret: '1XJn4suMEy56ddwTitMCPBag9beipx4O0bTInfgDhp5xv',
+}
+
+module.exports = twitterKeys;
